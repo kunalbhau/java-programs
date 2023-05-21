dcc44b874537658bb5ebab7e1468d5fbefba6b66
kunal <84256660+kunalbhau@users.noreply.github.com>
2023-05-21 19:11:13 +0530

    Add files via upload

---
+++ write a java program to define a class define instance method and overload them and use for dynamic memory invocation.java
@@ -0,0 +1,27 @@
+// write a java program to define a class define instance
+//methods and overload them and use them for dynamic memory
+ //allocation//
+
+ import java.util.*;
+
+ class add{
+    void display(int a, int b)
+    {
+        int c =a+b;
+        system.out.println("the sum of"+ a + "&" + b +"is" +"c");
+
+    }
+    void display(double a, double b);
+
+    {
+        double c= a+b;
+        system.out.println("the sum of" +a + "&" + b +"is" +"c")
+
+    }
+ }
+{
+    add obj =new add();
+    obj.display=(10,20);
+    obj.display =(10.2, 20.2);
+
+} 
