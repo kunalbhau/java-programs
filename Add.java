2998d940f76b1bb4ddca1577915630cc21dd7bdf
kunal <84256660+kunalbhau@users.noreply.github.com>
2023-08-14 21:45:12 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+
+public class Add{
+    public static void main (String [] args)
+    {
+        System.out.println("The sum of the numbers is");
+         
+        int num1 =4;
+        int num2 =4;
+        int num3 =6;
+       int sum = num1+ num2+ num3;
+       
+       System.out.println(sum);
+
+    
+    }
+
+}
