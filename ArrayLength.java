882809ad15420709ba409a6859abea1b5b153b08
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2024-12-23 20:00:40 +0530

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,15 @@
+
+public class ArrayLength {
+
+	
+	  public static void main(String args [])
+	  {
+		   int [] arr = { 1,2,3,4,5,6,7,8,9};
+		   int len =0;
+		   
+		   
+		   for (int i =0; i< arr.length; i++)
+			   len++;
+		   System.out.println(len);
+	  }
+}
