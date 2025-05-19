610d9cf83c38bf45bccd97f2cadebaa97105f34d
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-05-19 12:43:46 +0530

    Add files via upload

---
+++ CountOfNum1.java
@@ -0,0 +1,16 @@
+class CountOfNum1
+ {
+ public static void main(String args [])
+ {
+ long num =7432338;
+ long count =0;
+ 
+ while (num > 0)
+ {
+	 num = num /10;
+ count ++;
+ System.out.println(count);
+ 
+ }
+ }
+ }
