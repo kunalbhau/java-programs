4c27880af8210f7da1aaa740688cca59e675afeb
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-05-19 13:06:14 +0530

    Add files via upload

---
+++ PronicNum.java
@@ -0,0 +1,22 @@
+class PronicNum
+ {
+ public static void main(String args [])
+ {
+  int num = 7;
+  int temp;
+  {
+  for (int i = 1; i < num; i++)
+  {
+  temp = i *(i+1);
+  if(temp == num)
+  {
+  System.out.println("It is a pronic number");
+  }
+  else
+  {
+  System.out.println("It is not a pronic number");
+  }
+  }
+  }
+  }
+  }
