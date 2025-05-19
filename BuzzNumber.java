df521ed53794fc1ba376aac23845831d1f33fb6a
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-05-19 13:03:05 +0530

    Add files via upload

---
+++ BuzzNumber.java
@@ -0,0 +1,15 @@
+public class BuzzNumber
+ {
+ public static void main(String args [])
+ {
+ int num =7;
+ if(num % 7 ==0 || num % 10 ==7)
+ {
+ System.out.println("The number is a buzz number");
+ }
+ else
+ {
+ System.out.println("The number is not a buzz number");
+ }
+ }
+ }
