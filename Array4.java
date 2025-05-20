a9f080e2d43c8705a930cdbb8d9dc232ef7520d1
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-05-20 10:25:40 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,8 @@
+public class Array4
+ {
+ public static void main(String args [])
+ {
+ int [] arr = new int [5877834898];
+ System.out.println(arr);
+ }
+ }
---
+++ Array5.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Array5
+ {
+ public static void main(String args[])
+ {
+ int arr [] = new int[6];
+ arr[0] =16;
+ arr[1] = 15;
+ arr[2]= 14;
+ arr[3]= 19;
+ arr[4] = 20;
+ arr[5]= 21;
+ 
+ System.out.println(arr[0]);
+ System.out.println(arr[1]);
+ System.out.println(arr[2]);
+ System.out.println(arr[3]);
+ System.out.println(arr[4]);
+ System.out.println(arr[5]);
+ System.out.println(Arrays.toString(arr));
+ 
+ }
+ }
