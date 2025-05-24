c37e0bb0da85ee56bfa1e5d795138ab9e26c4337
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-05-24 09:13:16 +0530

    Add files via upload
    Finding Largest element in an array

---
+++ MaxElementArray24.java
@@ -0,0 +1,17 @@
+ public class MaxElementArray24
+  {
+  public static void main(String args[])
+   {
+   int max =0;
+   int [] arr ={1,2,3,4,5};
+   
+   for (int i =0; i< arr.length; i++)
+    {
+	 if (arr[i] > 0)
+	 {
+	 max = arr[i];
+	 }
+	 }
+	 System.out.println(max);
+	 }
+	 }
