5a5a4bedfca9a2a0c3ce989e4c00adb128102d90
kunal kale <84256660+kunalbhau@users.noreply.github.com>
2025-06-15 13:13:35 +0530

    Add files via upload

---
+++ Refact.java
@@ -0,0 +1,15 @@
+class Refact
+  {
+  public static void main(String args[])
+  {
+  System.out.println(fact(5));
+  }
+  public static int fact(int num)
+  {
+  if (num ==0 )
+  {
+  return 1;
+  }
+  return num  * fact (num -1);
+  }
+  }
